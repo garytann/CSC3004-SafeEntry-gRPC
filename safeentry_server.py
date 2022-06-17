23b2589ba3da3c80b1dddc5fe853f8d7ff1f128b
gary <garytannjy@gmail.com>
2022-06-17 15:07:56 +0800

    added sample json file to store key-value pair

--- safeentry_server.py
+++ safeentry_server.py
@@ -4,18 +4,22 @@ import logging
 import grpc
 import safeentry_pb2
 import safeentry_pb2_grpc
+import json
+from datetime import datetime
 
 
 class SafeEntry(safeentry_pb2_grpc.SafeEntryServicer):
-
     '''Function to take user checkin info
     Returns a CheckInReply with success or failure'''
+
     def CheckIn(self, request, context):
         print(request.nric, request.location)
+        addData(request.nric, request.location, request.checkin)
         return safeentry_pb2.CheckInReply(message="Success")
 
     '''Function to take user checkin info
     Returns a CheckInReply with success or failure'''
+
     def CheckOut(self, request, context):
         print(request.nric)
         return safeentry_pb2.CheckOutReply(message="Success")
@@ -27,6 +31,21 @@ def serve():
     server.add_insecure_port('[::]:50051')
     server.start()
     server.wait_for_termination()
+
+
+def addData(nric, location, dateTime):
+    dateTime = datetime.now()
+    datas = {
+        nric: [
+            {
+                "location": location,
+                "checkInDateTime": dateTime.strftime("%H:%M:%S"),
+            }
+        ]
+    }
+
+    with open("datas/datas.json", "w") as out:
+        json.dump(datas, out)
 
 
 if __name__ == '__main__':
