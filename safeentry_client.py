c02b3bdf19eea6b9252da56c13df54d142d7231f
gary <garytannjy@gmail.com>
2022-06-18 22:37:44 +0800

    return a list of location from past 14 days

--- safeentry_client.py
+++ safeentry_client.py
@@ -11,24 +11,29 @@ import safeentry_pb2_grpc
 
 '''Function to log a checkin to server
 Args: gRPC stub and variables to pass to server: user name, NRIC, building name, checkin datetime'''
-def checkin(stub, name, nric, location, checkin):
 
+
+def checkin(stub, name, nric, location, checkin):
     response = stub.CheckIn(safeentry_pb2.CheckInRequest(name=name, nric=nric, location=location, checkin=checkin))
     print(response.message)
+
 
 '''Function to log a checkout from existing location to server
 Checkout doesn't need name, just needs NRIC to find user in database
 Args: gRPC stub and variables to pass to server: NRIC, checkout datetime'''
+
+
 def checkout(stub, nric, checkout):
-      
     response = stub.CheckOut(safeentry_pb2.CheckOutRequest(nric=nric, checkout=checkout))
     print(response.message)
 
+
 '''Function to checkin a group of people
 Args: gRPC stub and variables to pass to server: list of names, list of NRICs, location and checkin datetime'''
+
+
 def checkin_group(stub, nameList: list, nricList: list, location, checkin):
-    
-    # List inputs must be list 
+    # List inputs must be list
     # If the input is only a single string input ("Dave" instead of ["Dave"]), 
     # the server would split the string into multiple characters and think they are individual people (["D", "A", "V", "E"])
     if type(nameList) != list or type(nricList) != list:
@@ -42,11 +47,15 @@ def checkin_group(stub, nameList: list, nricList: list, location, checkin):
         print("Number of names and NRIC do not match")
         return
 
-    response = stub.CheckInGroup(safeentry_pb2.GroupInRequest(name=nameList, nric=nricList, location=location, checkin=checkin))
+    response = stub.CheckInGroup(
+        safeentry_pb2.GroupInRequest(name=nameList, nric=nricList, location=location, checkin=checkin))
     print(response.message)
+
 
 '''Function to checkout a group of people
 Args: gRPC stub and variables to pass to server: list of NRICs and checkout datetime'''
+
+
 def checkout_group(stub, nricList: list, checkout):
     if type(nricList) != list:
         print("Failure")
@@ -56,13 +65,27 @@ def checkout_group(stub, nricList: list, checkout):
     response = stub.CheckOutGroup(safeentry_pb2.GroupOutRequest(nric=nricList, checkout=checkout))
     print(response.message)
 
+
 '''Function to get list of visited locations
 Args: gRPC stub and variables to pass to server: nric'''
+
+
 def get_history(stub, nric):
     response = stub.LocationHistory(safeentry_pb2.HistoryRequest(nric=nric))
-    
+
     for location in response.locations:
         print(location)
+
+
+'''Function to check for positive covid cases
+Requirement: within the past 14 days
+'''
+
+
+def check_cases(stub):
+    response = stub.CheckCases(safeentry_pb2.LocationCheck())
+    print(response)
+
 
 ########################
 ### PYTHON FUNCTIONS ###
@@ -70,22 +93,24 @@ def get_history(stub, nric):
 
 '''Function to get current datetime
 Returns string with current date and time (e.g. 24/12/2018, 04:59:31)'''
+
+
 def get_current_datetime():
     now = datetime.now()
     return now.strftime("%d/%m/%Y, %H:%M:%S")
 
 
 if __name__ == "__main__":
-
-
-    #Establishing channels and stubs before every function incurs overhead
-    #So establish once here and reuse
+    # Establishing channels and stubs before every function incurs overhead
+    # So establish once here and reuse
     with grpc.insecure_channel('localhost:50051') as channel:
         stub = safeentry_pb2_grpc.SafeEntryStub(channel)
 
         # checkin(stub, "TJ", "S087896T", "SIT", get_current_datetime())
 
         # checkout(stub, "S087896T", get_current_datetime())
+
+        check_cases(stub)
 
         testgroup = ["S1", "S2"]
         testnames = ["A", "B"]
--- safeentry_server.py
+++ safeentry_server.py
@@ -5,22 +5,23 @@ import grpc
 import safeentry_pb2
 import safeentry_pb2_grpc
 import json
-from datetime import datetime
+from datetime import date, timedelta, datetime
 
 
 class SafeEntry(safeentry_pb2_grpc.SafeEntryServicer):
-    
     '''Function to take user checkin info
     Returns a CheckInReply with success or failure'''
-    #TODO Ensure IC is 9 digits and starts with 'S' or 'T' and ends with letter
+
+    # TODO Ensure IC is 9 digits and starts with 'S' or 'T' and ends with letter
     def CheckIn(self, request, context):
         print(request.nric, request.location)
-        #TODO Add name as well into JSON
+        # TODO Add name as well into JSON
         addData(request.nric, request.location, request.checkin)
         return safeentry_pb2.CheckInOutReply(message="Success")
 
     '''Function to take user checkin info
     Returns a CheckOutReply with success or failure'''
+
     def CheckOut(self, request, context):
         print(request.nric, request.checkout)
         updateData(request.nric, request.checkout)
@@ -28,16 +29,18 @@ class SafeEntry(safeentry_pb2_grpc.SafeEntryServicer):
 
     '''Function to take checkin info of groups
     Returns a CheckInReply with success or failure'''
+
     def CheckInGroup(self, request, context):
-        #request.nric and request.name are lists
+        # request.nric and request.name are lists
         for ic in request.nric:
             print(ic)
         return safeentry_pb2.CheckInOutReply(message="Success")
 
     '''Function to checkout groups
     Returns a CheckOutReply with success or failure'''
+
     def CheckOutGroup(self, request, context):
-        #request.nric is a list
+        # request.nric is a list
         for ic in request.nric:
             print(ic)
         return safeentry_pb2.CheckInOutReply(message="Success")
@@ -45,11 +48,16 @@ class SafeEntry(safeentry_pb2_grpc.SafeEntryServicer):
     '''Function to return list of locations visited
     Using user's NRIC, traverse through JSON db and collate all locations
     Returns list of locations'''
+
     def LocationHistory(self, request, context):
-        #TODO JSON logic to find locations
-        #TODO Check if NRIC exists
+        # TODO JSON logic to find locations
+        # TODO Check if NRIC exists
         locations = ["SP", "NYP", "Tekong"]
         return safeentry_pb2.HistoryReply(locations=locations)
+
+    def CheckCases(self, request, context):
+        getLocation()
+        return safeentry_pb2.LocationReply(location="success")
 
 
 def serve():
@@ -58,6 +66,7 @@ def serve():
     server.add_insecure_port('[::]:50051')
     server.start()
     server.wait_for_termination()
+
 
 ######################
 ### JSON FUNCTIONS ###
@@ -84,7 +93,8 @@ def addData(nric, location, dateTime):
     with open("datas/datas.json", "w") as out:
         out.write(json_obj)
 
-#TODO remember the last location checkedin to check out from
+
+# TODO remember the last location checkedin to check out from
 def updateData(nric, dateTime):
     with open("datas/datas.json", "r") as f:
         file = json.load(f)
@@ -97,11 +107,27 @@ def updateData(nric, dateTime):
     with open("datas/datas.json", "w") as out:
         out.write(json_obj)
 
-##TODO check if the location has covid in the past 14 days
-def checkCases(nric, location):
+
+# TODO get the list of location that is within 14 days from current date
+def getLocation():
+    locationList = []
+    now = datetime.now()
+
+    ## 2022/6/4
+    cur = now - timedelta(days=14)
+
     with open("datas/location.json", "r") as f:
         locations = json.load(f)
+    for i in locations:
+        locationDate = locations[i]["Date"]
+        locationDate = datetime.strptime(locationDate, '%d/%m/%Y')
 
+        if (locationDate > cur):
+            locationList.append(i)
+
+    print(locationList)
+
+    return locationList
 
 
 if __name__ == '__main__':
