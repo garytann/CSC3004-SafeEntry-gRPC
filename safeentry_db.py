d3a091ebac07821e296a4f1fef0547942b38fae5
gary <garytannjy@gmail.com>
2022-06-22 00:39:03 +0800

    #1) added check for existing nric and append the new location. #2) only update the last index of the list during checkout

--- safeentry_db.py
+++ safeentry_db.py
@@ -1,29 +1,46 @@
-from asyncio.windows_events import NULL
+# from asyncio.windows_events import NULL
 import json
 from datetime import timedelta, datetime
 
+
 class Database():
-    
     '''Read and open json file first for optimised performance'''
+
     def __init__(self):
         self.data_file = json.load(open("datas/datas.json", "r"))
         self.location_file = json.load(open("datas/location.json", "r"))
 
     '''Function to add new SafeEntry data into datas.json
     Args: user's name, nric, visiting location and check in datetime'''
-    def addData(self, name, nric, location, dateTime):
-        datas = {
-            nric: [
-                {
-                    "name": name,
-                    "location": location,
-                    "checkInDateTime": dateTime,
-                    "checkOutDateTime": ""
-                }
-            ]
-        }
 
-        self.data_file.update(datas)
+    def addData(self, name, nric, location, dateTime):
+
+        if nric in self.data_file:
+            print('NRIC already exist')
+            cur = self.data_file[nric]
+
+            #temporary store key and values
+            new_location = {
+                "location": location,
+                "checkInDateTime": dateTime,
+                "checkOutDateTime": ""
+            }
+
+            cur.append(new_location)
+
+        else:
+            datas = {
+                nric: [
+                    {
+                        "name": name,
+                        "location": location,
+                        "checkInDateTime": dateTime,
+                        "checkOutDateTime": ""
+                    }
+                ]
+            }
+
+            self.data_file.update(datas)
 
         json_obj = json.dumps(self.data_file, indent=4)
 
@@ -32,12 +49,13 @@ class Database():
 
     '''Function to update existing SafeEntry entry with check out datetime
     Args: user's nric and check out datetime'''
+
     def updateData(self, nric, dateTime):
         selected_user = self.data_file[nric]
 
         # TODO remember the last location checkedin to check out from
-        selected_user[0]["checkOutDateTime"] = dateTime
-        print(selected_user[0]["checkOutDateTime"])
+        selected_user[-1]["checkOutDateTime"] = dateTime
+        print(selected_user[:-1]["checkOutDateTime"])
 
         json_obj = json.dumps(self.data_file, indent=4)
 
@@ -46,10 +64,10 @@ class Database():
 
     def addLocation(self, location, dateTime):
         location = {
-            location: 
-            {
-                "Date": dateTime
-            }
+            location:
+                {
+                    "Date": dateTime
+                }
         }
 
         self.location_file.update(location)
@@ -61,9 +79,10 @@ class Database():
 
     '''Function to get list of locations visited by a Covid case within past 14 days
     Returns list of locations'''
+
     def getLocation(self):
         locationList = []
-        
+
         ## 2022/6/4
         now = datetime.now()
         cur = now - timedelta(days=14)
@@ -78,16 +97,17 @@ class Database():
         print(locationList)
 
         return locationList
-    
+
     '''Function to get list of locations visited by user
     Returns list of locations'''
+
     def getVisited(self, nric):
         locationList = []
-        
+
         ## 2022/6/4
         now = datetime.now()
         cur = now - timedelta(days=14)
-        
+
         for i in self.data_file[nric]:
             locationList.append(i["location"])
 
@@ -98,12 +118,13 @@ class Database():
     '''Function to check if user has visited an infected location within past 14 days
     Args: nric of user and list of infected locations
     Returns TODO'''
+
     def getCases(self, nric, infectedLocation: list):
         LocationList = []
 
         now = datetime.now()
         cur = now - timedelta(days=14)
-      
+
         visitedLocation = self.data_file[nric]
         # print(visitedLocation)
         for j in visitedLocation:
@@ -122,5 +143,6 @@ class Database():
 
     '''Reusable function to check if NRIC exists in datas.json
     Returns true if so'''
+
     def nricExists(self, nric):
         return False
