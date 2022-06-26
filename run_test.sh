3072292a98e83e8f353e7700e92afe8faf020ff5
gary <garytannjy@gmail.com>
2022-06-26 15:31:11 +0800

    added shell script for MAC user testing

---
+++ run_test.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+echo "***** Starting bash shell script test ******"
+python test_clients/safeentry_client_test_loop.py &
+python test_clients/safeentry_client_test_loop.py &
+python test_clients/safeentry_client_test_loop.py &
+python test_clients/safeentry_client_test_loop.py &
+python test_clients/safeentry_client_test_loop.py &
+python test_clients/safeentry_client_test_loop.py &
+python test_clients/safeentry_client_test_loop.py &
+python test_clients/safeentry_client_test_loop.py &
+python test_clients/safeentry_client_test_loop.py &
+python test_clients/safeentry_client_test_loop.py &
+python test_clients/safeentry_client_test_loop.py &
+python test_clients/safeentry_client_test_loop.py &
+python test_clients/safeentry_client_test_loop.py &
+python test_clients/safeentry_client_test_loop.py &
+python test_clients/safeentry_client_test_loop.py &
+python test_clients/safeentry_client_test_loop.py &
+python test_clients/safeentry_client_test_loop.py &
+python test_clients/safeentry_client_test_loop.py &
+echo "***** Automated bash shell script END ******"
+
